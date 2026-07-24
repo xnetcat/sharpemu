f8589e1d23ccb047a930d85db270793e171d9c83
kuba <xnetcat.dev@gmail.com>
2026-07-25 01:12:38 +0200

    [Video] Fix compressed UI textures and movie cropping
    Ported from origin/upstream-prep-1-avplayer-gen5-abi 7cac498 (clean).
    
    Width in the extended frame ABI describes the padded NV12 allocation, so
    report the visible image through the crop-right field instead of lying
    about the width (for a 378px movie in a 512px row, 512 - 134). Also
    enable textureCompressionBC on the created device so guest BC1-BC7
    textures can be sampled directly.

--- src/SharpEmu.Libs/VideoOut/VulkanVideoPresenter.cs
+++ src/SharpEmu.Libs/VideoOut/VulkanVideoPresenter.cs
@@ -4117,6 +4117,7 @@ internal static unsafe class VulkanVideoPresenter
                 ShaderStorageImageExtendedFormats = supportedFeatures.ShaderStorageImageExtendedFormats,
                 ShaderStorageImageReadWithoutFormat = supportedFeatures.ShaderStorageImageReadWithoutFormat,
                 ShaderStorageImageWriteWithoutFormat = supportedFeatures.ShaderStorageImageWriteWithoutFormat,
+                TextureCompressionBC = supportedFeatures.TextureCompressionBC,
                 RobustBufferAccess = supportedFeatures.RobustBufferAccess,
             };
 
@@ -4154,6 +4155,13 @@ internal static unsafe class VulkanVideoPresenter
                 Console.Error.WriteLine(
                     "[LOADER][WARN] GPU does not support shaderStorageImage(Read|Write)WithoutFormat " +
                     "translated shaders using unformatted storage image load/store will fail.");
+            }
+
+            if (!supportedFeatures.TextureCompressionBC)
+            {
+                Console.Error.WriteLine(
+                    "[LOADER][WARN] GPU does not support textureCompressionBC " +
+                    "guest BC1-BC7 textures cannot be sampled directly.");
             }
 
             var maintenance8Features = new PhysicalDeviceMaintenance8FeaturesKHR
